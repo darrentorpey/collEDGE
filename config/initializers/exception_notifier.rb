77b1eb192acb76522d01f14013cb47d0755c35d5
Darren Torpey <dct@punchbowlsoftware.com>
2010-02-20 13:15:08 -0500

    removing unneeded stuff

--- config/initializers/exception_notifier.rb
+++
@@ -1,4 +0,0 @@
-Lipsiadmin::Mailer::ExceptionNotifier.sender_address       = %("Exception Notifier" <exceptions@colledge.com>)
-Lipsiadmin::Mailer::ExceptionNotifier.recipients_addresses = %(help@colledge.com)
-Lipsiadmin::Mailer::ExceptionNotifier.email_prefix         = "[CollEDGE]"
-Lipsiadmin::Mailer::ExceptionNotifier.send_mail            = true
